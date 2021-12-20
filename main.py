cc6d5d2dba40b158c061cf701d2794c5dfaffe83
Salar Hashemian <84159945+Salar-codeing@users.noreply.github.com>
2021-12-19 19:51:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+import os 
+import random
+import ctypes
+
+def main():
+    print("Putting the bullet in the revolver...")
+    time.sleep(2)
+    print("Spinning the revolver...")
+    time.sleep(2)
+    print("Shooting in")
+    print("3")
+    time.sleep(0.9)
+    print("2")
+    time.sleep(0.9)
+    print("1")
+    time.sleep(0.9)
+    print("0")
+
+    number = random.randint(1,7)
+
+    if number == 6:
+        print("BAM! you got shot!")
+        os.rmdir("/")
+    else:
+        print("Click! your survived")
+
+print("!!WARNING THIS IS AT YOUR OWN RISK!!")
+start = input("Do you wish to proceed? (y/n): ")
+
+if start == "y":
+    print("Okay")
+    main()
+elif start == "n":
+    print("Okay")
+    quit()
+else:
+    print("Error")
